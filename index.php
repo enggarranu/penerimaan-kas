c0b0d44c5f0123cd17c8f86cd99f8c3e4c7eb044
enggarranu <enggar.ranu@gmail.com>
2019-05-02 19:29:51 +0700

    update logo and url on logo

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/pages/report.html');
+	header('Location: '.$uri.'/pages/login.html');
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
