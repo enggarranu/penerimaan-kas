047d00d5369a2ccbb04f7438e89da054f8784f9e
enggarranu <enggar.ranu@gmail.com>
2019-05-02 11:24:26 +0700

    fix url on cloud

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/penerimaan-kas/pages/report.html');
+	header('Location: '.$uri.'/pages/report.html');
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
